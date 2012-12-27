ccbe613bbbc68d2c802eed982266db84946ab394
Evan Tahler <evantahler@gmail.com>
2012-12-27 10:33:38 -0800

    Update examples/auth.js

--- examples/auth.js
+++ examples/auth.js
@@ -13,26 +13,18 @@ var taskrabbit = new taskrabbitPrototype({
 });
 
 http.createServer(function (req, res) {
-
   if(req.url == "/auth"){
-
     res.writeHead(301, {'Location': taskrabbit.oauthClientURL() });
     res.end('Go Here: ' + taskrabbit.oauthClientURL() + '\n');
-
   }else if(req.url.indexOf("/oauth_callback") >= 0){
-
     taskrabbit.oauthAuthenticateClient(req, function(error, access_token){
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Your oAuth access token is: ' + access_token + '\n');
     });
-
   }else{
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('Go Away\n');
-
   }
-  
 }).listen(8080, '0.0.0.0');
 
 console.log("server running");
